bdf785ca176d951c803af88aeffd1ddee1e9b70b
Дмитрий Хероим <enchtein7@gmail.com>
2025-05-14 11:47:06 +0300

    create project

---
+++ TestTask/TestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaskApp.swift
+//  TestTask
+//
+//  Created by Дмитрий Хероим on 14.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct TestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
